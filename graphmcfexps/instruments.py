6191c8ef3dc7c544805961def990c38ef872593f
Karasiari <124217577+Karasiari@users.noreply.github.com>
2025-12-13 17:53:13 +0300

    Update instruments.py

--- graphmcfexps/instruments.py
+++ graphmcfexps/instruments.py
@@ -208,8 +208,7 @@ def subdivide_flows_by_paths(flow, demand_indices_by_group, ungrouped_demands, i
         flow_graph = flow_graphs[i]
 
         # Sort demands by capacity in descending order
-        #sorted_demand_indices = sorted(demand_indices, key=lambda idx: ungrouped_demands[idx].capacity, reverse=True)
-        sorted_demand_indices = demand_indices
+        sorted_demand_indices = sorted(demand_indices, key=lambda idx: ungrouped_demands[idx].capacity, reverse=True)
 
         # Process each demand in descending order of capacity
         for demand_index in sorted_demand_indices:
@@ -265,8 +264,7 @@ def fulfill_remaining_demands(graph_copy, ungrouped_demands, demand_indices_by_g
         source, sink = i_to_source_target[i]
 
         # Sort demands by capacity in descending order
-        #sorted_demand_indices = sorted(demand_indices, key=lambda idx: ungrouped_demands[idx].capacity, reverse=True)
-        sorted_demand_indices = demand_indices
+        sorted_demand_indices = sorted(demand_indices, key=lambda idx: ungrouped_demands[idx].capacity, reverse=True)
 
         # Process each demand in descending order of capacity
         for demand_index in sorted_demand_indices:
