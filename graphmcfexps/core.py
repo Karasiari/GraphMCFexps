91a505c90efe13f855e8e49b3c4167f4e416cfbc
Karasiari <124217577+Karasiari@users.noreply.github.com>
2025-12-24 15:18:45 +0300

    Update core.py

--- graphmcfexps/core.py
+++ graphmcfexps/core.py
@@ -26,8 +26,8 @@ class GraphMCFexps:
         self.demands_laplacian = self._get_laplacian(self.demands_graph)
         
         # поскольку граф будет меняться в экспериментах по расширению - храним исходный вариант
-        self.multigraph_initial = self.multigraph.copy(deep=True)
-        self.graph_initial = self.graph.copy(deep=True)
+        self.multigraph_initial = self.multigraph.copy()
+        self.graph_initial = self.graph.copy()
 
         # последние вычисленные alpha и "усредненная" L_alpha
         self.alpha: Optional[float] = None
@@ -269,8 +269,8 @@ class GraphMCFexps:
         Восстановление self.multigraph из self.multigraph_initial
         """
 
-        self.multigraph = self.multigraph_initial.copy(deep=True)
-        self.graph = self.graph_initial.copy(deep=True)
+        self.multigraph = self.multigraph_initial.copy()
+        self.graph = self.graph_initial.copy()
 
     # ---------- решения основных задач на графе ----------
     
